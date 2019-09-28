8a99efc21acfca627f3795ec310454756766ee22
laijianbo <laijianbo@juxinli.com>
2019-09-28 09:26:52 +0800

    a

--- src/com/phei/netty/protocol/netty/client/HeartBeatReqHandler.java
+++ src/com/phei/netty/protocol/netty/client/HeartBeatReqHandler.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013-2018 Lilinfeng.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,61 +15,50 @@
  */
 package com.phei.netty.protocol.netty.client;
 
-import io.netty.channel.ChannelHandlerAdapter;
-import io.netty.channel.ChannelHandlerContext;
-
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
 import com.phei.netty.protocol.netty.MessageType;
 import com.phei.netty.protocol.netty.struct.Header;
 import com.phei.netty.protocol.netty.struct.NettyMessage;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Lilinfeng
  * @version 1.0
  * @date 2014年3月15日
  */
+@Slf4j
 public class HeartBeatReqHandler extends ChannelHandlerAdapter {
-
-    private static final Log LOG = LogFactory.getLog(HeartBeatReqHandler.class);
 
     private volatile ScheduledFuture<?> heartBeat;
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)
-            throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
         NettyMessage message = (NettyMessage) msg;
         // 握手成功，主动发送心跳消息
-        if (message.getHeader() != null
-                && message.getHeader().getType() == MessageType.LOGIN_RESP
-                .value()) {
-            heartBeat = ctx.executor().scheduleAtFixedRate(
-                    new HeartBeatReqHandler.HeartBeatTask(ctx), 0, 5000,
-                    TimeUnit.MILLISECONDS);
-        } else if (message.getHeader() != null
-                && message.getHeader().getType() == MessageType.HEARTBEAT_RESP
-                .value()) {
-            LOG.info("Client receive server heart beat message : ---> "
-                            + message);
-        } else
+        if (message.getHeader() != null && message.getHeader().getType() == MessageType.LOGIN_RESP.value()) {
+            heartBeat = ctx.executor().scheduleAtFixedRate(new HeartBeatTask(ctx), 0, 5000, TimeUnit.MILLISECONDS);
+        } else if (message.getHeader() != null && message.getHeader().getType() == MessageType.HEARTBEAT_RESP.value()) {
+            log.info(String.format("Client receive server heart beat message : ---> %s", message));
+        } else {
             ctx.fireChannelRead(msg);
+        }
     }
 
     private class HeartBeatTask implements Runnable {
         private final ChannelHandlerContext ctx;
 
-        public HeartBeatTask(final ChannelHandlerContext ctx) {
+        HeartBeatTask(final ChannelHandlerContext ctx) {
             this.ctx = ctx;
         }
 
         @Override
         public void run() {
             NettyMessage heatBeat = buildHeatBeat();
-            LOG.info("Client send heart beat messsage to server : ---> "
-                            + heatBeat);
+            log.info(String.format("Client send heart beat messsage to server : ---> %s", heatBeat));
             ctx.writeAndFlush(heatBeat);
         }
 
@@ -83,8 +72,7 @@ public class HeartBeatReqHandler extends ChannelHandlerAdapter {
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-            throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         cause.printStackTrace();
         if (heartBeat != null) {
             heartBeat.cancel(true);
--- src/com/phei/netty/protocol/netty/struct/NettyMessage.java
+++ src/com/phei/netty/protocol/netty/struct/NettyMessage.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013-2018 Lilinfeng.
- *  
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,54 +15,17 @@
  */
 package com.phei.netty.protocol.netty.struct;
 
+import lombok.Data;
+
 /**
  * @author lilinfeng
- * @date 2014年3月14日
  * @version 1.0
+ * @date 2014年3月14日
  */
+@Data
 public final class NettyMessage {
 
     private Header header;
 
     private Object body;
-
-    /**
-     * @return the header
-     */
-    public final Header getHeader() {
-	return header;
-    }
-
-    /**
-     * @param header
-     *            the header to set
-     */
-    public final void setHeader(Header header) {
-	this.header = header;
-    }
-
-    /**
-     * @return the body
-     */
-    public final Object getBody() {
-	return body;
-    }
-
-    /**
-     * @param body
-     *            the body to set
-     */
-    public final void setBody(Object body) {
-	this.body = body;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-	return "NettyMessage [header=" + header + "]";
-    }
 }
